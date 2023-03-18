ee6e1e722ac89a8fcac6717179a2f4c86363d1ea
Speedforce1230 <gamedhall@gmail.com>
2023-03-18 14:32:02 +0530

    Removed and changed Variables
    Removed redundant index functions. Renamed games to images.

--- GameTimeSite/urls.py
+++ GameTimeSite/urls.py
@@ -4,18 +4,6 @@ from . views import *
 # path function is used to define a path for the urls of web pages
 urlpatterns = [
     path('', index),
-    # path("Valorant/", index2),
-    # path('GOTG/', index3),
-    # path('R6S/', index4),
-    # path('DIV/', index5),
-    # path('Outriders/', index6),
-    # path('TW3/', index7),
-    # path('AAT/', index8),
-    # path('TF2/', index9),
-    # path('GAAC/', index10),
-    # path('Sekiro/', index11),
-    # path('ER/', index12),
-    # path('FC6/', index13),
-    path('about/', index14),
+    path('about/', index2),
 ]
 
--- GameTimeSite/views.py
+++ GameTimeSite/views.py
@@ -4,78 +4,15 @@ from . models import GameTime
 from image_url import image_url
 # GameTime.objects.all() is used to retrieve all objects created by the admin page on the website
 # It contains all the important fields like name, time played etc.
-games = GameTime.objects.all()
+images = GameTime.objects.all()
 
 # Index functions are used to render .html files and also images
 
 
 def index(request):
     return render(request, 'index.html',
-                  {'games': games})
-
+                  {'images': images})
 
 def index2(request):
-    val = image_url["valorant"]
-    return render(request, 'valorant.html', {'val': val})
-
-
-def index3(request):
-    gotg = image_url["guardians"]
-    return render(request, 'gotg.html',
-                  {'gotg': gotg})
-
-
-def index4(request):
-    r6 = image_url["rainbowsix"]
-    return render(request, 'r6.html',
-                  {'r6': r6})
-
-
-def index5(request):
-    dv = image_url["division"]
-    return render(request, 'division.html', {"dv": dv})
-
-
-def index6(request):
-    out = image_url['outriders']
-    return render(request, 'outriders.html', {'out': out})
-
-
-def index7(request):
-    tw3 = image_url['witcher3']
-    return render(request, 'witcher3.html', {'tw3': tw3})
-
-
-def index8(request):
-    aat = image_url['aceattorney']
-    return render(request, 'aat.html', {'aat': aat})
-
-
-def index9(request):
-    tf2 = image_url['titanfall2']
-    return render(request, 'tf2.html', {'tf2': tf2})
-
-
-def index10(request):
-    gaac = image_url['greataceattorney']
-    return render(request, 'gaac.html', {'gaac': gaac})
-
-
-def index11(request):
-    sekiro = image_url['sekiro']
-    return render(request, 'sekiro.html', {'sekiro': sekiro})
-
-
-def index12(request):
-    er = image_url["eldenring"]
-    return render(request, 'eldenring.html', {'er': er})
-
-
-def index13(request):
-    fc6 = image_url['fc6']
-    return render(request, 'fc6.html', {'fc6': fc6})
-
-
-def index14(request):
     return render(request, 'about.html')
 
