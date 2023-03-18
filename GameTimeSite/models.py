8c3958bf1bdac4495f8af9b0277fb9dbe67490d2
Speedforce1230 <gamedhall@gmail.com>
2023-03-18 13:48:32 +0530

    Changes Necessary

--- GameTimeSite/models.py
+++ GameTimeSite/models.py
@@ -2,8 +2,5 @@ from django.db import models
 
 
 class GameTime(models.Model):
-    name = models.CharField(max_length=30)
-    hours_played = models.IntegerField()
-    status = models.CharField(max_length=12)
     image_url = models.CharField(max_length=2083)
 
