cdb943bb336175f09030cacb71ff013f59b21ad2
Speedforce1230 <gamedhall@gmail.com>
2023-04-16 15:59:41 +0530

    Made rows and column logic better, dynamic rows with two columns instead of one giant row

--- ImageApp/views.py
+++ ImageApp/views.py
@@ -9,8 +9,9 @@ homepage_images = HomeImage.objects.all()
 
 class DisplayHTML(View):
     def index(self, request):
+        HomePageImgTuples = [(homepage_images[i-2], homepage_images[i-1]) for i in range(2, len(homepage_images) + 2, 2)]
         return render(request, 'index.html',
-                    {'home_images': homepage_images,})
+                    {'home_images': HomePageImgTuples,})
 
     def index2(self, request):
         return render(request, 'about.html')
@@ -20,19 +21,23 @@ class DisplayHTML(View):
 class SendImagesToJS(View):
     def send_fashion_img(self, request):
         images = FashionImage.objects.all()
+        length = len(images)
         data = {"images": [{"name": image.name, "file_extension": image.file_extension} for image in images],
-                "MEDIA_URL": settings.MEDIA_URL_2}
+                "MEDIA_URL": settings.MEDIA_URL_2, "length": length,}
+        print(data)
         return JsonResponse(data)
 
     def send_product_img(self, request):
         images = ProductImage.objects.all()
+        length = len(images)
         data = {"images": [{"name": image.name, "file_extension": image.file_extension} for image in images],
-                "MEDIA_URL": settings.MEDIA_URL_3}
+                "MEDIA_URL": settings.MEDIA_URL_3, "length": length,}
         return JsonResponse(data)
     
     def send_home_img(self, request):
+        length = len(homepage_images)
         data = {"images": [{"name": home_img.name, "file_extension": home_img.file_extension} for home_img in homepage_images],
-                "MEDIA_URL": settings.MEDIA_URL}
+                "MEDIA_URL": settings.MEDIA_URL, "length": length,}
         return JsonResponse(data)
 
 
--- static/ImageApp/js/display_button_images.js
+++ static/ImageApp/js/display_button_images.js
@@ -7,32 +7,30 @@ export function send_image_to_webpage(url){
     .then(Response => Response.json())
     .then(data => {
         const imageContainer = document.getElementById("image-container");
-        imageContainer.innerHTML = ""; 
-        const rowElement = document.createElement("div");
-        rowElement.classList.add("row");
-        rowElement.id = "dynamic-card-row";
-        
-
-        data.images.forEach(image => {
+        imageContainer.innerHTML = "";
+        let rowElement = "";
+        for (var i = 0; i < data.length; i++){
+            if (i % 2 == 0){
+                rowElement = document.createElement("div");
+                rowElement.classList.add("row");
+                rowElement.id = "dynamic-card-row";
+                imageContainer.appendChild(rowElement);
+            }
+            let image = data.images[i];
             const imgElement = document.createElement("img");
             imgElement.src = data.MEDIA_URL + image.name + "." + image.file_extension;
             imgElement.id = image.name;
 
-
             const columnElement = document.createElement("div");
-            columnElement.id = "dynamic-card-columns";
             columnElement.classList.add("col");
+            columnElement.id = "dynamic-card-columns";
             columnElement.appendChild(imgElement);
-
-
             rowElement.appendChild(columnElement);
-            
+
             imgElement.addEventListener("click", function(){
                 onImageClick(image.name, image.file_extension, data.MEDIA_URL);
             })
-        });
-        imageContainer.appendChild(rowElement);
-        
+        }
     })
     .catch(error => console.error(error))
 }
